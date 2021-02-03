f08bf88067c22ab6f6ff54587edf5f1a82384696
Shaine Berube <shaine@vorakortech.io>
2021-02-02 17:20:41 -0700

    Moving database folder into src folder because Rust doesn't like relative path.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::fs;
+
 fn main() {
     println!("Hello, world!");
+    create_schema();
+}
+
+
+fn create_schema() {//schema: String) {
+    // This function is meant to choose a schema and create it in the database.
+    let paths = fs::read_dir("./database/schemas/").unwrap();
+    for path in paths {
+        println!("File name: {}", path.unwrap().path().display());
+    }
 }
